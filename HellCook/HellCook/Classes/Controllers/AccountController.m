099b582aa2eb2c5a7fd24ec227f746dd653d1184
Batistuta <terry1984@163.com>
2013-08-05 21:25:25 +0800

    我的收藏 修改了下拉功能

--- HellCook/HellCook/Classes/Controllers/AccountController.m
+++ HellCook/HellCook/Classes/Controllers/AccountController.m
@@ -21,6 +21,7 @@
 #import "NetManager.h"
 #import "MyCollectionController.h"
 #import "UIImage+Blurring.h"
+//#import "MyselfRootViewController.h"
 #import "AppDelegate.h"
 #import "UIZoomNavigationController.h"
 #import "MainController.h"
