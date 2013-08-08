19c67d11b47f3ec22faf8270c04ed1a395c04135
Batistuta <terry1984@163.com>
2013-08-08 09:55:06 +0800

    followcell

---
+++ HellCook/HellCook/Classes/Views/MyFollowTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MyFollowTableViewCell.h
+//  HellCook
+//
+//  Created by lxw on 13-8-7.
+//  Copyright (c) 2013年 panda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyFollowTableViewCell : UITableViewCell
+
+@end
