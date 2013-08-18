ed26332b860a79daf868059928d2166a4d75dd65
Batistuta <terry1984@163.com>
2013-08-18 22:38:38 +0800

    阶段性的功能都做完了

---
+++ HellCook/HellCook/Classes/Views/RecipeDetail/RecipeSendCommentView.h
@@ -0,0 +1,13 @@
+//
+//  RecipeSendCommentView.h
+//  HellCook
+//
+//  Created by lxw on 13-8-18.
+//  Copyright (c) 2013年 panda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeSendCommentView : UIView
+
+@end
---
+++ HellCook/HellCook/Classes/Views/RecipeDetail/RecipeSendCommentView.m
@@ -0,0 +1,31 @@
+//
+//  RecipeSendCommentView.m
+//  HellCook
+//
+//  Created by lxw on 13-8-18.
+//  Copyright (c) 2013年 panda. All rights reserved.
+//
+
+#import "RecipeSendCommentView.h"
+
+@implementation RecipeSendCommentView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
